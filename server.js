4ce8fcf583d5e93c9a5eaa189f742465bd4fae1e
vitor <vitor.d3.alves@gmail.com>
2025-08-22 01:42:00 -0300

    note: Front-end atualizado

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// servir pasta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Server rodando na porta ${port}`);
+});
